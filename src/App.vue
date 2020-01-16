02f52a8ab46cf6ae8e68c6afbdd7f219e9fb6074
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-16 08:37:05 -1000

    login and signup components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
