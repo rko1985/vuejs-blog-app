51b95812f839eaf3c7449e9faeb7fd01d6427c60
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-01-23 08:44:15 -1000

    create article

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import Signup from './views/Signup.vue'
 import Article from './views/Article.vue'
-
+import CreateArticle from './views/CreateArticle.vue'
 
 Vue.use(Router)
 
@@ -27,6 +27,10 @@ const router = new Router({
         {
             path: '/article/:id',
             component: Article
+        },
+        {
+            path: '/articles/create',
+            component: CreateArticle
         }
         
     ]
